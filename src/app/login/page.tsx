200a085c8f7e67b7c3ee86e3f79cdb757b95f6a9
anjali790 <anjaliruby790@gmail.com>
2023-09-29 15:23:29 +0530

    signup and login done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,74 +10,36 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 export default function LoginPage() {
-  // const router = useRouter();
+  const router = useRouter();
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  // const [user, setUser] = useState({
-  //   email: "",
-  //   password: "",
-  // });
-
-  // const [buttonDisabled, setButtonDisabled] = useState(false);
-
-  // const [loading, setLoading] = useState(false);
-
-  // const onLogin = async () => {
-  //   try {
-  //     // setLoading(true);
-  //     const response = await axios.post("api/users/login", user);
-  //     console.log("LOgin success", response.data);
-  //     toast.success("Login success");
-  //     router.push("/profile");
-  //   } catch (error: any) {
-  //     console.log("Login failed");
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (user.email.length > 0 && user.password.length > 0) {
-  //     setButtonDisabled(false);
-  //   } else {
-  //     setButtonDisabled(true);
-  //   }
-  // }, [user]);
-
-
-
-
-
-
-    const router = useRouter();
-    const [user, setUser] = React.useState({
-        email: "",
-        password: "",
-       
-    })
-    const [buttonDisabled, setButtonDisabled] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
-
-
-    const onLogin = async () => {
-        try {
-            setLoading(true);
-            const response = await axios.post("/api/users/login", user);
-            console.log("Login success", response.data);
-            toast.success("Login success");
-            router.push("/profile");
-        } catch (error:any) {
-            console.log("Login failed", error.message);
-            toast.error(error.message);
-        } finally{
-        setLoading(false);
-        }
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        if(user.email.length > 0 && user.password.length > 0) {
-            setButtonDisabled(false);
-        } else{
-            setButtonDisabled(true);
-        }
-    }, [user]);
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <>
